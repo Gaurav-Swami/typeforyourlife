ea238763162508005c5c356a99cc669b8d4a353e
Gaurav-Swami <gauravswami0404@gmail.com>
2025-03-06 23:41:14 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import {Routes,Route} from "react-router-dom"
+import './App.css'
+import Test from "./pages/Test"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element = {<Test/>}/>
+      </Routes>
+      
+    </>
+  )
+}
+
+export default App
